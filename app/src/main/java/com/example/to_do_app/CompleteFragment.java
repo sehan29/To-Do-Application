efb22ae446027da14f615d4c03981c5c98220f19
sehan29 <sehangamage708@gmail.com>
2024-05-05 12:39:14 +0530

    Added Few To-Do Features

---
+++ app/src/main/java/com/example/to_do_app/CompleteFragment.java
@@ -0,0 +1,19 @@
+package com.example.to_do_app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CompleteFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_complete, container, false);
+    }
+}
