61f78906b2d3863fa65e8e6c0c254037aa249b14
sehan29 <sehangamage708@gmail.com>
2024-05-05 13:58:42 +0530

    Added Home Pade Basic Features

--- app/src/main/java/com/example/to_do_app/HomeFragment.java
+++ app/src/main/java/com/example/to_do_app/HomeFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class HomeFragment extends Fragment {
 
@@ -15,5 +17,22 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Find the ListView by its ID
+        ListView listView = view.findViewById(R.id.listViewTasks);
+
+        // Sample data to display in the ListView
+        String[] tasks = {"Web Application Development", "Programming II", "Flutter", "Python Tutorial", "Android Learning", "LMS Dev", "Statistics"};
+
+        // Create an ArrayAdapter using the string array and a default list item layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, tasks);
+
+        // Set the adapter to the ListView
+        listView.setAdapter(adapter);
     }
 }
