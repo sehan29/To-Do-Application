80f4a175f66e3e73a7bb021a97298e03fc69c0a2
sehan29 <sehangamage708@gmail.com>
2024-05-09 23:54:55 +0530

    Make Few Styles Changes

--- app/src/main/java/com/example/to_do_app/Login_Page.java
+++ app/src/main/java/com/example/to_do_app/Login_Page.java
@@ -6,22 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login_Page extends AppCompatActivity {
 
     Button login;
+    TextView text_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
         login = findViewById(R.id.login_btn);
+        text_login = findViewById(R.id.registration_id);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent login_intent = new Intent(getApplicationContext(), Dashboard.class);
                 startActivity(login_intent);
+            }
+        });
+
+        text_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registration = new Intent(getApplicationContext(), Registration.class);
+                startActivity(registration);
             }
         });
 
--- app/src/main/java/com/example/to_do_app/Registration.java
+++ app/src/main/java/com/example/to_do_app/Registration.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Registration extends AppCompatActivity {
+public class   Registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
